5c291f3bb60f4da34fc020f9d38c848c424b8d6e
Kleak <me@kleak.dev>
2020-11-12 15:43:42 +0100

    fix binding

--- packages/fibonacci_c_binding/lib/fibonacci.dart
+++ packages/fibonacci_c_binding/lib/fibonacci.dart
@@ -1,17 +1,17 @@
 import 'dart:ffi' as ffi;
 
-typedef fibonacci_func = ffi.Void Function();
+typedef fibonacci_func = ffi.Int32 Function(ffi.Int32);
 
-typedef Fibonacci = void Function();
+typedef Fibonacci = int Function(int);
 
 ffi.DynamicLibrary _libFibonacci;
 Fibonacci _fibonacci;
 
 void initLibrary(String libraryPath) {
   _libFibonacci = ffi.DynamicLibrary.open(libraryPath);
-  _fibonacci = _libFibonacci.lookup<ffi.NativeFunction<fibonacci_func>>('fibonacci').asFunction();
+  _fibonacci = _libFibonacci.lookupFunction<fibonacci_func, Fibonacci>('fibonacci');
 }
 
-void nativeFibonacci(int n) => _fibonacci();
+void nativeFibonacci(int n) => _fibonacci(n);
 
 int dartFibonacci(int n) => (n > 2) ? (dartFibonacci(n - 1) + dartFibonacci(n - 2)) : 1;
